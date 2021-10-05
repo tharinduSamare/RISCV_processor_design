0b4d2e30828ddfb821eba43508a7fcaa2e80d85f
Ravinduabey <ravinduabey@gmail.com>
2021-10-05 16:35:10 +0530

    Modify module: change the module pcAdd from sequential to combinational

--- src/pcAdd.sv
+++ src/pcAdd.sv
@@ -1,12 +1,9 @@
 module pcAdd(
-    input   logic           clk,
     input   logic   [31:0]  pcOld,
 
     output  logic   [31:0]  pcNew
     );
 
-    always_ff @( clk ) begin : incrementPc
-        pcNew   <= pcOld + 3'd4;
-    end
+    assign pcNew = pcOld + 3'd4;
 
 endmodule: pcAdd
