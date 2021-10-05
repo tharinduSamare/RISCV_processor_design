5d3e09126ef6a81377403fc776371c2d0ffa895c
Ravinduabey <ravinduabey@gmail.com>
2021-10-05 16:33:03 +0530

    Modify functionality: changed how the pc branch logic unit works

--- src/pcBranchLogic.sv
+++ src/pcBranchLogic.sv
@@ -1,14 +1,49 @@
 module pcBranch(
-    input   logic           clk,
-    // input   logic           branch,
-    input   logic   [31:0]  pcOld,
-    input   logic   [31:0]  branchPc,
-
-    output  logic   [31:0]  pcNew
+    input   logic [2:0] funct3,
+    input   logic       branch,
+    output  logic [2:0] branchType,
+    output  logic       takeBranch
     );
 
-    always_ff @( clk ) begin
-        pcNew   <=  pcOld + branchPc;
+    typedef enum logic [ 2:0 ]{
+        BEQ,
+        BNE,
+        BLT,
+        BLE,
+        BGE,
+        BLTU,
+        BGEU
+    } branch_;
+
+    branch_ _;
+    always_comb begin : branchLogic
+        if (branch) begin
+            takeBranch <= '1;    
+            if (funct3 == 3'b000) begin
+                branchType <= BEQ;
+            end
+            else if (funct3 == 3'b001) begin
+                branchType <= BNE;            
+            end
+            else if (funct3 == 3'b010) begin
+                branchType <= BLT;           
+            end
+            else if (funct3 == 3'b011) begin
+                branchType <= BLE;            
+            end
+            else if (funct3 == 3'b100) begin
+                branchType <= BGE;            
+            end
+            else if (funct3 == 3'b101) begin
+                branchType <= BLTU;            
+            end
+            else if (funct3 == 3'b111) begin
+                branchType <= BGEU;            
+            end
+        end
+        else begin
+            takeBranch <= '0;
+        end
     end
 
 endmodule: pcBranch
