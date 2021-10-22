3a9425e62702c71f5a22071bddbb4fb54c80c26d
unknown <tharindu.samare123@gmail.com>
2021-10-22 15:07:31 +0530

    mem_controller for lw, lh, lb sw,sh, sb instructions partially designed. need to push extend unit

---
+++ src/mem_controller.sv
@@ -0,0 +1,17 @@
+module mem_controller 
+#(
+    ADDRESS_WIDTH = 8,
+    DATA_WIDTH = 32
+)
+(
+    input logic clk,wrEn,
+    input logic [ADDRESS_WIDTH-1:0]address,
+    input logic [DATA_WIDTH-1:0]data
+
+);
+
+
+
+
+
+endmodule : mem_controller
