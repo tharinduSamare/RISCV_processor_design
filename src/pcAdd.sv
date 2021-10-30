a6aed6db320b518ad5e380d404267f7c84bda566
Ravindu Abeygunawardena <ravinduabey@gmail.com>
2021-10-30 15:47:43 +0530

    Change portName: changed the port name from pcNew to pcNewFour

--- src/pcAdd.sv
+++ src/pcAdd.sv
@@ -1,9 +1,9 @@
 module pcAdd(
     input   logic   [31:0]  pcOld,
 
-    output  logic   [31:0]  pcNew
+    output  logic   [31:0]  pcNewFour
     );
 
-    assign pcNew = pcOld + 3'd4;
+    assign pcNewFour = pcOld + 3'd4;
 
 endmodule: pcAdd
