9e93d6d9294db26e9022b145b256c747b20b2480
Ravindu Abeygunawardena <ravinduabey@gmail.com>
2021-10-31 11:02:06 +0530

    Add module: pcBranchAnd

---
+++ src/pcBranchAnd.sv
@@ -0,0 +1,9 @@
+module pcBranchAnd (
+    input logic ifBranch, //from the pcBranchType unit
+    input logic branchIn, //from the control unit
+
+    output logic branchOut
+);
+    
+    //get the and of the two
+endmodule
