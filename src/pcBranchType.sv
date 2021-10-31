e2cd857c7ce40539076cac11240d6625fdad1015
Ravindu Abeygunawardena <ravinduabey@gmail.com>
2021-10-31 23:40:29 +0530

    Add feature: check branch in the module itself

--- src/pcBranchType.sv
+++ src/pcBranchType.sv
@@ -1,16 +1,18 @@
-module pcBranchType #(
+module pcfunc3 #(
     parameter REG_COUNT     = 32,
     parameter REG_SIZE      = $clog2(REG_COUNT)
 )
 (
-    input logic signed [31 : 0]   rs1,
-    input logic signed [31 : 0]   rs2,
-    input logic [2:0]                   branchType,
+    input logic                         branch, // from control unit
+    input logic [2:0]                   func3,
+
+    input logic signed [31 : 0]         rs1,
+    input logic signed [31 : 0]         rs2,
 
     output logic                        branchN
 );
 
-typedef enum logic [ 2:0 ]{
+    typedef enum logic [ 2:0 ]{
         BEQ  = 3'b000,
         BNE  = 3'b001,
         BLT  = 3'b100,
@@ -40,27 +42,29 @@ typedef enum logic [ 2:0 ]{
     end
 
     always_comb begin : check_branch
-    /*  
-        Depending on the type of branch 
-        required comparison is carried out
-    */
-        if (branchType == BEQ && rs1 == rs2)begin
-            branchN = '1;
-        end
-        else if (branchType == BNE && rs1 != rs2)begin
-            branchN = '1;
-        end
-        else if (branchType == BLT && rs1 < rs2)begin
-            branchN = '1;
-        end
-        else if (branchType == BGE && rs1 >= rs2)begin
-            branchN = '1;
-        end
-        else if (branchType == BLTU && rs1_un < rs2_un)begin
-            branchN = '1;
-        end
-        else if (branchType == BGEU && rs1_un >= rs2_un)begin
-            branchN = '1;
+        if (branch) begin
+        /*  
+            Depending on the type of branch 
+            required comparison is carried out
+        */
+            if (func3 == BEQ && rs1 == rs2)begin
+                branchN = '1;
+            end
+            else if (func3 == BNE && rs1 != rs2)begin
+                branchN = '1;
+            end
+            else if (func3 == BLT && rs1 < rs2)begin
+                branchN = '1;
+            end
+            else if (func3 == BGE && rs1 >= rs2)begin
+                branchN = '1;
+            end
+            else if (func3 == BLTU && rs1_un < rs2_un)begin
+                branchN = '1;
+            end
+            else if (func3 == BGEU && rs1_un >= rs2_un)begin
+                branchN = '1;
+            end
         end
         else begin
             branchN = '0;
