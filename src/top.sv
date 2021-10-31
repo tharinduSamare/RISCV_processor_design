e32e9c78c852ef493b645ec3e9d339bf8cfa3ef9
MuhammedImdaad <muhammedimdaad16@gmail.com>
2021-10-31 18:26:57 +0530

    added PC mux

--- src/top.sv
+++ src/top.sv
@@ -20,7 +20,7 @@ logic pcWrite;
 logic [INSTRUCTION_WIDTH-1:0] pcIF;
 logic [INSTRUCTION_WIDTH-1:0] pcInc;
 logic [INSTRUCTION_WIDTH-1:0] jumpAddr;
-
+logic takeBranch;
 pc PC(
     .clk(clk),
     .pcIn(pcIn),
@@ -33,10 +33,10 @@ pcAdd PC_Adder (
     .pcNewFour(pcInc)
 );
 
-// mux PC_Select (
-//     .dataIn(pcInc, jumpAddr),
-//     .dataOut(pcIn)
-// );
+// pc mux //
+assign takeBranch = branchCU & branchS;
+assign pcIn = (takeBranch) ? jumpAddr : pcInc;
+
 
 
 ///// IM /////
@@ -68,7 +68,7 @@ pipilineRegister_IF_ID IF_ID_Register(
 
 ///// Control Unit /////
 logic [OP_CODE_WIDTH-1:0] opCode = instructionID[31:25];
-logic jump, jumpReg, branch, memReadID, memWriteID, memtoRegID, regWriteID;
+logic jump, jumpReg, branchCU, memReadID, memWriteID, memtoRegID, regWriteID;
 logic [1:0] aluSrc1ID,aluSrc2ID;
 logic [1:0] aluOpID;
 
@@ -76,7 +76,7 @@ control_unit CU(
     .opCode,
     .jump,
     .jumpReg,
-    .branch,
+    .branch(branchCU),
     .memRead(memReadID),
     .memWrite(memWriteID),
     .memtoReg(memtoRegID),
@@ -107,10 +107,70 @@ reg_file Reg_File(
     .regB_out(rs2DataID)
 );
 
+///// Branch Logic Module /////
+///// Branch Exe Module /////
+logic branchS;
 //// Extender Module /////
     // input logic [31:0]instruction,
     // output logic signed [31:0] I_immediate, S_immediate, SB_immediate, U_immediate, UJ_immediate
 // immediate_extend(
 //     .instruction()
+// );
+
+///// ID/EX Pipeline Register /////
+
+    // // other signals to exe stage
+    // input logic [7 : 0]     func7_IDIn,
+    // input logic [2 : 0]     func3_IDIn,
+    // input logic [15 : 0]    read1_IDIn,
+    // input logic [15 : 0]    read2_IDIn,
+    
+    // input logic [31 : 0]    I_imme_IDIn,
+    // input logic [31 : 0]    S_imme_IDIn,
+    // input logic [31 : 0]    U_imme_IDIn,
+
+    // input logic [4 : 0]     rd_IDIn,
+    // input logic [4 : 0]     rs1_IDIn,
+    // input logic [4 : 0]     rs2_IDIn,
+
+    
+    // //pipelined outputs
+    // //to execution stage
+    // output logic [1:0]      aluSrc_ID1Out,
+    // output logic [1:0]      aluSrc2_IDOut,
+    // output logic [1:0]      aluOp_IDOut,
+
+    
+    // // to memory stage
+    // output logic             memWrite_IDOut,
+    // output logic             memRead_IDOut,
+    
+    // // to writeback stage
+    // output logic             regWrite_IDOut,
+    // output logic             memToRegWrite_IDOut,
+
+    // // other signals to exe stage
+    // output logic [7 : 0]    func7_IDOut,
+    // output logic [2 : 0]    func3_IDOut,
+    // output logic [15 : 0]   read1_IDOut,
+    // output logic [15 : 0]   read2_IDOut,
+    
+    // output logic [31 : 0] I_imme_IDOut,
+    // output logic [31 : 0] S_imme_IDOut,
+    // output logic [31 : 0] U_imme_IDOut,
+
+    // output logic [4 : 0] rd_IDOut,
+    // output logic [4 : 0] rs1_IDOut,
+    // output logic [4 : 0] rs2_IDOut
+// pipilineRegister_ID_EX ID_EX_Register(
+//     .aluSrc1_IDIn(aluSrc1ID),
+//     .aluSrc2_IDIn(aluSrc2ID),
+//     .aluOp_IDIn(aluOpID),
+
+//     .memWrite_IDIn(memWriteID),
+//     .memRead_IDIn(memReadID),
+//     .regWrite_IDIn(regWriteID),
+//     .memToRegWrite_IDIn(memtoRegID),
+
 // );
 endmodule
