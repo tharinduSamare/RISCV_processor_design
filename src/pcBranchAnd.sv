bcbbcbbef5aa3ebdff58cb152102408c38888ae5
MuhammedImdaad <muhammedimdaad16@gmail.com>
2021-11-01 00:06:41 +0530

    added branch unit

--- src/pcBranchAnd.sv
+++
@@ -1,9 +0,0 @@
-module pcBranchAnd (
-    input logic ifBranch, //from the pcBranchType unit
-    input logic branchIn, //from the control unit
-
-    output logic branchOut
-);
-    
-    assign branchOut = ifBranch && branchIn;
-endmodule
