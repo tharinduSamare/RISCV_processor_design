f6aaff5136f3d5237d8f2c1aba45a1550c5c1b6d
Ravindu Abeygunawardena <ravinduabey@gmail.com>
2021-11-01 16:30:21 +0530

    Add removed pipeline registers

---
+++ src/pipilineRegister_IF_ID.sv
@@ -0,0 +1,18 @@
+module pipilineRegister_IF_ID( 
+    input logic             clk,
+
+    input logic [31 : 0] pcIn,
+    input logic [31 : 0] instructionIn,
+
+    input logic harzardIF_ID_Write,
+    input logic flush,
+
+    output logic [31 : 0] pcOut,
+    output logic [31 : 0] instructionOut
+);
+
+    always_ff @( posedge clk ) begin : IF_ID_REGISTER
+        pcOut               <=      pcIn;
+        instructionOu       <=      instructionIn;
+    end
+endmodule
