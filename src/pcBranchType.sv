c89c2673ab44807b0fd93bb3835696f9336c9158
Ravindu Abeygunawardena <ravinduabey@gmail.com>
2021-11-17 16:58:13 +0530

    Fix merge conflict: removed content added due to merge conflict

--- src/pcBranchType.sv
+++ src/pcBranchType.sv
@@ -19,7 +19,7 @@ module pcBranchType #(
     } branch_;
 
     branch_ _;
-<<<<<<< HEAD
+
 
     logic   [31:0]  rs1_un;
     logic   [31:0]  rs2_un;
@@ -39,48 +39,13 @@ module pcBranchType #(
         end
     end
 
+
     always_comb begin : check_branch
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-    always_comb begin : check_branch
->>>>>>> 7976b3dfae5b02a687b022f3bd9966bcf4017e1e
     /*  
         Depending on the type of branch 
         required comparison is carried out
     */
-        if (branchType == BEQ && read1 == read2)begin
-            branchN = '1;
-        end
-        else if (branchType == BNE && read1 != read2)begin
-            branchN = '1;
-        end
-        else if (branchType == BLT && read1 < read2)begin
-            branchN = '1;
-        end
-        else if (branchType == BGE && read1 >= read2)begin
-            branchN = '1;
-        end
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 7976b3dfae5b02a687b022f3bd9966bcf4017e1e
-        else if (branchType == BLTU && read1 < read2) begin
-            branchN = '1;
-        end
-        else if (branchType == BGEU && read1 >= read2) begin
-<<<<<<< HEAD
-=======
-        else if (branchType == BLTU && rs1_un < rs2_un)begin
-            branchN = '1;
-        end
-        else if (branchType == BGEU && rs1_un >= rs2_un)begin
->>>>>>> 0a17a7d (Bug fixes: fixed the unsigned bug is pcbranchtype module and the name issue in pcSelect)
-            branchN = '1;
-=======
         if (branch) begin
-=======
->>>>>>> 3bb1e50 (Bug fix: fixed the module name issue and changed the condition statement for the branching)
         /*  
             Depending on the type of branch 
             required comparison is carried out
@@ -103,22 +68,11 @@ module pcBranchType #(
             else if (branch && func3 == BGEU && rs1_un >= rs2_un)begin
                 branchN = '1;
             end
-<<<<<<< HEAD
->>>>>>> e2cd857 (Add feature: check branch in the module itself)
-=======
+
             branchN = '1;
->>>>>>> 7976b3dfae5b02a687b022f3bd9966bcf4017e1e
         end
         else begin
             branchN = '0;
         end
-<<<<<<< HEAD
-=======
-            else begin
-                branchN = '0;
-            end
->>>>>>> 3bb1e50 (Bug fix: fixed the module name issue and changed the condition statement for the branching)
-=======
->>>>>>> 7976b3dfae5b02a687b022f3bd9966bcf4017e1e
     end
 endmodule
