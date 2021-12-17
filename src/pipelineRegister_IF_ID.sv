236da0b360cd2c574ba45b90d857c4d8473caa4f
unknown <tharindu.samare123@gmail.com>
2021-12-17 17:20:16 +0530

    bug fix: next instruction after branch and mem_read, mem write works correctly after delay

--- src/pipelineRegister_IF_ID.sv
+++ src/pipelineRegister_IF_ID.sv
@@ -14,12 +14,15 @@ module pipelineRegister_IF_ID(
     always_ff @( posedge clk or negedge rstN ) begin : IF_ID_REGISTER
         if (~rstN) begin
             pcOut               <=      '0;
-            instructionOut      <=      {25'b0, 7'b0010011};
+            instructionOut      <=      {25'b0, 7'b0010011}; // addi x0 x0 0
         end
         else begin
 		  if (~harzardIF_ID_Write) begin
 			// pcOut               <=      '0;
-            instructionOut      <=      {25'b0, 7'b0010011}; // addi x0 x0 0
+            // instructionOut      <=      {25'b0, 7'b0010011}; // addi x0 x0 0
+            // instructionOut      <=      32'hFF5FF06F; // addi x0 x0 0
+			pcOut               <=      pcOut;
+            instructionOut      <=      instructionOut; // addi x0 x0 0
 		  end
 		  else begin
             pcOut               <=      pcIn;
