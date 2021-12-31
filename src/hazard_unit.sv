725f83a226317d5652918eff411d072556a8445f
unknown <tharindu.samare123@gmail.com>
2021-12-31 22:02:23 +0530

    added comments

--- src/hazard_unit.sv
+++ src/hazard_unit.sv
@@ -14,76 +14,75 @@ logic mem_op_stall,branch_stall, jumpreg_stall;
 logic stall;
 
 typedef enum logic [2:0]{
-    idle = 3'd0,
-    before_read_mem = 3'd1,
-    read_mem = 3'd2,
-    before_write_mem = 3'd3,
-    write_mem = 3'd4
-}state_t;
+    idle = 3'd0, // memory is idle
+    before_read_mem = 3'd1, // before memory load operation
+    read_mem = 3'd2, // during memory load operation
+    before_write_mem = 3'd3, // before memory store operation
+    write_mem = 3'd4 // during memory store operation
+}mem_stall_state_t;
 
-state_t current_state, next_state;
+mem_stall_state_t current_mem_stall_state, next_mem_stall_state;
 
-assign mem_op_stall = ((current_state != idle) | ID_Ex_MemRead | ID_Ex_MemWrite)?1'b1:1'b0;
-assign branch_stall = (branchCU & regWriteEX &  rdEx!=0 & ((rs1ID == rdEx)|(rs2ID == rdEx)))? 1'b1:1'b0;
-assign jumpreg_stall = (jumpRegCU  & regWriteEX &  rdEx!=0 & (rs1ID == rdEx))? 1'b1:1'b0;
+assign mem_op_stall = ((current_mem_stall_state != idle) | ID_Ex_MemRead | ID_Ex_MemWrite)?1'b1:1'b0;   // stall if memory read / memory write happens.
+assign branch_stall = (branchCU & regWriteEX &  rdEx!=0 & ((rs1ID == rdEx)|(rs2ID == rdEx)))? 1'b1:1'b0; // stall during branching if there are data dependencies.
+assign jumpreg_stall = (jumpRegCU  & regWriteEX &  rdEx!=0 & (rs1ID == rdEx))? 1'b1:1'b0; // stall during jump operation if there are data dependencies.
 
-assign stall = (mem_op_stall | branch_stall | jumpreg_stall)? 1'b1:1'b0;
+assign stall = (mem_op_stall | branch_stall | jumpreg_stall)? 1'b1:1'b0; // stall the pipeline if any of the above reasons satisfied.
 
-assign branchHU = (~branch_stall & branchCU)? 1'b1:1'b0;
-assign jumpRegHU = (~jumpreg_stall & jumpRegCU)? 1'b1:1'b0;
+assign branchHU = (~branch_stall & branchCU)? 1'b1:1'b0; // send branch control signal to muxes.
+assign jumpRegHU = (~jumpreg_stall & jumpRegCU)? 1'b1:1'b0; // send jump control signal to muxes.
 
 assign ID_Ex_enable = (stall)? 1'b0:1'b1;
+assign pcStall = (stall)? 1'b1:1'b0;
 assign PC_write = (stall)? 1'b0:1'b1;
 assign IF_ID_write = (stall)? 1'b0:1'b1;
-assign pcStall = (stall)? 1'b1:1'b0;
 
+// handle stalls during memory operations
 always_ff @(posedge clk) begin
     if (~rstN)begin
-        current_state <= idle;
+        current_mem_stall_state <= idle;
     end
     else begin
-        current_state <= next_state;
+        current_mem_stall_state <= next_mem_stall_state;
     end
 end
 
 always_comb begin
-    next_state = current_state;
+    next_mem_stall_state = current_mem_stall_state;
 
-    case (current_state) 
+    case (current_mem_stall_state) 
         idle: begin
-            if (ID_Ex_MemRead) begin
-                next_state = before_read_mem;
+            if (ID_Ex_MemRead) begin // identify memory read at execution stage
+                next_mem_stall_state = before_read_mem;
             end
-            else if (ID_Ex_MemWrite) begin
-                next_state = before_write_mem;
+            else if (ID_Ex_MemWrite) begin // identify memory write at execution stage
+                next_mem_stall_state = before_write_mem;
             end
         end
 
-        before_read_mem: begin
+        before_read_mem: begin // wait until memory read starts
             if (mem_ready == 1'b0) begin
-                next_state = read_mem;
+                next_mem_stall_state = read_mem;
             end
         end
         
-        read_mem: begin
+        read_mem: begin // wait during memory read operation
             if (mem_ready) begin
-                next_state = idle;
+                next_mem_stall_state = idle;
             end
         end
 
-        before_write_mem: begin
+        before_write_mem: begin // wait until memory write start
             if (mem_ready == 1'b0) begin
-                next_state = write_mem;
+                next_mem_stall_state = write_mem;
             end
         end
         
-        write_mem: begin
+        write_mem: begin // wait during memory write 
             if (mem_ready) begin
-                next_state = idle;
+                next_mem_stall_state = idle;
             end
         end
-
-
 
     endcase
 end
--- src/immediate_extend.sv
+++ src/immediate_extend.sv
@@ -4,17 +4,17 @@ module immediate_extend
     output logic signed [31:0] I_immediate, S_immediate, SB_immediate, U_immediate, UJ_immediate
 );
 
+// instructoin     |    31    |30         20|19           12|    11     |      10       |4             1|     0     |
+// I_immediate  => |                — inst[31] —                        |  inst[30:25]  | inst[24:21]   | inst[20]  |
+// S_immediate  => |                — inst[31] —                        |  inst[30:25]  | inst[11:8]    |  inst[7]  | 
+// SB_immediate => |            — inst[31] —                | inst[7]   |  inst[30:25]  | inst[11:8]    |     0     | 
+// U_immediate  => | inst[31] | inst[30:20] | inst[19:12]   |                        — 0 —                          |
+// UJ_immediate => |     — inst[31] —       | inst[19:12]   | inst[20] | inst[30:25]    | inst[24:21]   |     0     | 
+
 assign I_immediate = 32'(signed'(instruction[31:20]));
 assign S_immediate = 32'(signed'({instruction[31:25],instruction[11:7]}));
 assign SB_immediate = 32'(signed'({instruction[31],instruction[7], instruction[30:25], instruction[11:8],1'b0}));
 assign U_immediate = 32'(signed'({instruction[31:12],12'b0}));
 assign UJ_immediate = 32'(signed'({instruction[31], instruction[19:12], instruction[20],  instruction[30:21],1'b0}));
-
-// instructoin     |31        |30           |19
-// I_immediate  => | — inst[31] — | inst[30:25] | inst[24:21] | inst[20] |
-// S_immediate  => | — inst[31] — | inst[30:25] | inst[11:8] | inst[7] | 
-// SB_immediate => | — inst[31] — | inst[7] | inst[30:25] | inst[11:8] | 0 | 
-// U_immediate  => | inst[31] | inst[30:20] | inst[19:12] | — 0 — |
-// UJ_immediate => | — inst[31] — | inst[19:12] | inst[20] | inst[30:25] | inst[24:21] | 0 | 
 
 endmodule : immediate_extend
